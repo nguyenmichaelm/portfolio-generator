198e51fba5c3d05c93bf47acc9d6089a05933808
Michael Nguyen <nguyenmichaelm@yahoo.com>
2022-08-20 17:28:13 -0700

    install inquirer package and demonstrate how the interface works

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [user, github] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-fs.writeFile('index.html', generatePage(user, github), err => {
-  if (err) throw err;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
